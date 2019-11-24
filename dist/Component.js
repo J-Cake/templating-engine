54e2d760db9820ff460621092ef687b26e62fb56
JAKES-PC\Jacob Schneider <jakieschneider13@gmail.com>
2019-11-24 15:51:45 +1100

    Oops

--- dist/Component.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Component {
-    constructor(useState) {
-    }
-}
-exports.default = Component;
-//# sourceMappingURL=Component.js.map
--- dist/preferences.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const preferences = {
-    escapeChar: "\\"
-};
-exports.default = preferences;
-//# sourceMappingURL=preferences.js.map
